d663ceb5c6e39e418ef463612e030ce6773350c5
Alexander Smith <acsmith.dev@gmail.com>
2019-09-20 12:56:21 -0400

    add scss and style-resources

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,13 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
 
-  loading: { color: '#fff' },
+  loading: { color: '#2d862d' },
+
+  loadingIndicator: {
+    name: 'pulse',
+    color: '#b30000',
+    background: '#002db3'
+  },
 
   css: ['~/assets/app.css'],
 
@@ -26,7 +32,14 @@ export default {
     '@nuxtjs/axios',
     '@nuxtjs/pwa',
     '@nuxtjs/toast'
+    '@nuxtjs/style-resources'
   ],
+
+  styleResources: {
+    scss: [
+      './assets/*.scss'
+    ]
+  },
 
   toast: {
     position: 'top-center',
